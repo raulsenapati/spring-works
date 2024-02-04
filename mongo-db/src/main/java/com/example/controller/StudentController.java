fb108d69a7c701afbc32ddcd8d5bd0830fc01e4f
raulsenapati <rahulsen@buffalo.edu>
2024-02-04 00:03:53 -0500

    Postman Project, service updated

--- mongo-db/src/main/java/com/example/controller/StudentController.java
+++ mongo-db/src/main/java/com/example/controller/StudentController.java
@@ -1,13 +1,11 @@
 package com.example.controller;
 
 import com.example.entity.Student;
+import com.example.request.SortBy;
 import com.example.service.StudentService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,10 +25,79 @@ public class StudentController {
         return studentService.getAllStudents();
     }
 
+    @PostMapping("/create")
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
 
-    @Operation(summary = "Implemented by searching deptId due to DBRef Department")
+    @GetMapping("/getById/{id}")
+    public Student getStudentById(@PathVariable String id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PutMapping("/update")
+    public Student updateStudent(@RequestBody Student student) {
+        return studentService.updateStudent(student);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable String id) {
+        return studentService.deleteStudent(id);
+    }
+
+    @GetMapping("/studentsByName/{name}")
+    public List<Student> studentsByName(@PathVariable String name) {
+        return studentService.getStudentsByName(name);
+    }
+
+    @GetMapping("/studentsByNameAndMail")
+    public List<Student> studentsByNameAndMail(@RequestParam String name, @RequestParam String email) {
+        return studentService.getStudentsByNameAndMail(name, email);
+    }
+
+    @GetMapping("/studentsByNameOrMail")
+    public List<Student> studentsByNameOrMail(@RequestParam String name, @RequestParam String email) {
+        return studentService.getStudentsByNameOrMail(name, email);
+    }
+
+    @Operation(summary = "API to get list of all students - 1 indexed")
+    @GetMapping("/allWithPagination")
+    public List<Student> getAllWithPagination(@RequestParam int pageNo, @RequestParam int pageSize) {
+        return studentService.getAllWithPagination(pageNo, pageSize);
+    }
+
+    @Operation(summary = "API to get sorted list of all students")
+    @GetMapping("/allWithSorting")
+    public List<Student> allWithSorting(@RequestHeader(name = "direction", required = false) SortBy direction,
+                                        @RequestParam(defaultValue = "name", required = false) String property) {
+        return studentService.allWithSorting(direction, property);
+    }
+
+    @Operation(summary = "Implemented by searching deptId with DBRef Department")
     @GetMapping("/byDepartmentName")
     public List<Student> byDepartmentName(@RequestParam String deptName) {
         return studentService.byDepartmentName(deptName);
     }
+
+    @GetMapping("/bySubjectName")
+    public List<Student> bySubjectName(@RequestParam String subName) {
+        return studentService.bySubjectName(subName);
+    }
+
+    @GetMapping("/emailLike")
+    public List<Student> emailLike(@RequestParam String email) {
+        return studentService.emailLike(email);
+    }
+
+    @Operation(summary = "API to get list of students starting with Name")
+    @GetMapping("/nameStartsWith")
+    public List<Student> nameStartsWith(@RequestParam String name) {
+        return studentService.nameStartsWith(name);
+    }
+
+    @GetMapping("/byDepartmentId")
+    public List<Student> byDepartmentId(@RequestParam String deptId) {
+        return studentService.byDepartmentId(deptId);
+    }
+
 }
--- mongo-db/src/main/java/com/example/service/StudentService.java
+++ mongo-db/src/main/java/com/example/service/StudentService.java
@@ -3,7 +3,11 @@ package com.example.service;
 import com.example.entity.Student;
 import com.example.repository.DepartmentRepository;
 import com.example.repository.StudentRepository;
+import com.example.request.SortBy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -16,6 +20,9 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private static final String STUDENT_DELETED = "Student has been deleted successfully";
+
+    private static final String STUDENT_NOT_EXISTS = "Student probably does not exist";
     @Autowired
     StudentRepository studentRepository;
 
@@ -25,6 +32,33 @@ public class StudentService {
 //    @Autowired
 //    SubjectRepository subjectRepository;
 
+    public Student createStudent(Student student) {
+        if (student.getDepartment() != null) {
+            var department = departmentRepository.findByDepartmentNameAndLocation(
+                    student.getDepartment().getDepartmentName(),
+                    student.getDepartment().getLocation()
+            );
+            if (department != null) {
+                department = departmentRepository.save(department);
+            }
+            student.setDepartment(department);
+        }
+//        if(!CollectionUtils.isEmpty(student.getSubjects())) {
+//            subjectRepository.saveAll(student.getSubjects());
+//        }
+        return studentRepository.save(student);
+    }
+
+    /**
+     * { "id" : "65a5af3409eb5e18af59850f"}
+     */
+    public Student getStudentById(String id) {
+        var student = studentRepository.findById(id);
+        if (student.isPresent())
+            return student.get();
+        return null;
+    }
+
     /**
      * {}
      */
@@ -32,8 +66,80 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+    public Student updateStudent(Student student) {
+        if (student.getDepartment() != null) {
+            var department = departmentRepository.findByDepartmentNameAndLocation(
+                    student.getDepartment().getDepartmentName(),
+                    student.getDepartment().getLocation()
+            );
+            if (department != null) {
+                department = departmentRepository.save(department);
+            }
+            student.setDepartment(department);
+        }
+        return studentRepository.save(student);
+    }
+
+    public String deleteStudent(String id) {
+        var student = studentRepository.findById(id);
+        if (student.isPresent()) {
+            studentRepository.deleteById(id);
+            return STUDENT_DELETED;
+        }
+        return STUDENT_NOT_EXISTS;
+    }
+
     /**
+     * { "name" : "Yuvraj"}
+     */
+    public List<Student> getStudentsByName(String name) {
+        return studentRepository.findByName(name);
+        //return studentRepository.getByName(name);
+    }
+
+    /**
+     * { "name" : "John", "mail" : "john@gmail.com"}
+     */
+    public List<Student> getStudentsByNameAndMail(String name, String email) {
+        return studentRepository.findByNameAndEmail(name, email);
+    }
+
+    /**
+     * { "$or" : [{ "name" : "John-1"}, { "mail" : "steve@gmail.com"}]}
+     */
+    public List<Student> getStudentsByNameOrMail(String name, String email) {
+        return studentRepository.findByNameOrEmail(name, email);
+    }
+
+    /**
+     * skip(10).limit(10)
+     * skip value is calculated by method
+     */
+    public List<Student> getAllWithPagination(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+        return studentRepository.findAll(pageable)
+                .getContent();
+    }
+
+    /**
+     * db.getCollection('student').find({}).sort({name: -1}) //descending
+     */
+    public List<Student> allWithSorting(SortBy direction, String property) {
+        Sort sort = Sort.by(
+                SortBy.DESC.equals(direction)
+                        ? Sort.Direction.DESC
+                        : Sort.Direction.ASC,
+                //property can be multiple
+                property
+        );
+        return studentRepository.findAll(sort);
+    }
+
+
+    /**
+     * { "department.department_name" : "Computer and Information Science"} // old
      * { "department_name" : "Computer and Information Science"}
+     * then query-ed with department id
      */
     public List<Student> byDepartmentName(String deptName) {
         //return studentRepository.findByDepartmentDepartmentName(deptName);
@@ -46,5 +152,33 @@ public class StudentService {
                     );
         }
         return students;
+    }
+
+    /**
+     * { "subjects.subject_name" : "Reinforcement Learning"}
+     */
+    public List<Student> bySubjectName(String subName) {
+        return studentRepository.findBySubjectsSubjectName(subName);
+    }
+
+    /**
+     * { "mail" : /yahoo/ }
+     */
+    public List<Student> emailLike(String email) {
+        return studentRepository.findByEmailIsLike(email);
+    }
+
+    /**
+     * { "name" : /^John/ }
+     */
+    public List<Student> nameStartsWith(String name) {
+        return studentRepository.findByNameStartsWith(name);
+    }
+
+    /**
+     * { "department" : { "$java" : { "$ref" : "department", "$id" : "65a5b2a30dd6a27ad7c7c6eb" } } }
+     */
+    public List<Student> byDepartmentId(String deptId) {
+        return studentRepository.findByDepartmentId(deptId);
     }
 }
